cd760f9cd0b9592fafc664c1045455e8f472cdd4
Septa Cahyani <septacahyani22@gmail.com>
2023-09-02 22:32:23 +0700

    Update dan SoftDelete Barang

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const conn = require("./config/db");
 
 app.use(express.json());
 
+// 1. Menampilkan semua data barang yang ada
 app.get("/get-barang", function (req, res) {
   const queryStr = "SELECT * FROM barang WHERE deleted_at IS NULL";
   conn.query(queryStr, (err, results) => {
@@ -19,6 +20,7 @@ app.get("/get-barang", function (req, res) {
   });
 });
 
+// 2. Add Data Barang
 app.post("/store-barang", function (req, res) {
   const param = req.body;
   const name = param.name;
@@ -47,6 +49,7 @@ app.post("/store-barang", function (req, res) {
     }
   });
 });
+
 // 3. Get Barang by ID
 app.get("/get-barang-by-id", function (req, res) {
   const param = req.query;
@@ -65,6 +68,58 @@ app.get("/get-barang-by-id", function (req, res) {
       res.status(200).json({
         success: true,
         message: "Sukses mencari barang berdasarkan Id",
+        data: results,
+      });
+    }
+  });
+});
+
+// 4. Update Barang
+app.post("/update-barang", function (req, res) {
+  const param = req.body;
+  const id = param.id;
+  const name = param.name;
+  const stock = param.stock;
+  const queryStr =
+    "UPDATE barang SET name = ?, stock = ? WHERE id = ? AND deleted_at IS NULL";
+  const values = [name, stock, id];
+  conn.query(queryStr, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        success: false,
+        message: "Gagal mengubah barang",
+        data: null,
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Sukses mengubah data barang",
+        data: results,
+      });
+    }
+  });
+});
+
+// 5. Softdelete Data Barang
+app.post("/delete-barang", function (req, res) {
+  const param = req.body;
+  const id = param.id;
+  const queryStr = "UPDATE barang SET deleted_at = ? WHERE id = ?";
+  const now = new Date();
+  const values = [now, id];
+  conn.query(queryStr, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        success: false,
+        message: "Gagal menghapus barang",
+        data: null,
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Sukses menghapus data barang",
         data: results,
       });
     }
--- readme.txt
+++ readme.txt
@@ -7,4 +7,5 @@ API:
 1. Get Barang = menampilkan semua barang yang ada di tabel barang;
 2. Store Barang = menambahkan barang
 3. Get Barang Id = menampilkan data barang (detail) berdasarkan Id yang dicari
-4.
+4. Update Barang = mengubah data barang
+5. Delete Barang = softdelete (didatabase tidak dihapus dan memasukan waktu data di delete tapi ketika get barang untuk data yang pernah dihapus tidak ditampilkan saja)
