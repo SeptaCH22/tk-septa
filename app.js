ee477ab9231e61377d8a06beb9104eb293ca91b0
Septa Cahyani <septacahyani22@gmail.com>
2023-09-02 21:56:17 +0700

    Get Barang

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+const express = require("express");
+const app = express();
+const conn = require("./config/db");
+
+app.get("/get-barang", function (req, res) {
+  const queryStr = "SELECT * FROM barang WHERE deleted_at IS NULL";
+  conn.query(queryStr, (err, results) => {
+    if (err) {
+      res.error(err.sqlMessage, res);
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Sukses menampilkan data barang",
+        data: results,
+      });
+    }
+  });
+});
+
+app.listen(3000);
--- readme.txt
+++ readme.txt
@@ -1,6 +1,7 @@
 Package:
 1. Mysql
 2. Express
+3. nodemon: untuk mempermudah restart node.js
 
 API:
 1. Get Barang = menampilkan semua barang yang ada di tabel barang;
